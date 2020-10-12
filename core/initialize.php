0ebae5c7db0b64c92efeaf8fef1a432e3fc301ae
Adedeji Sikiru <adedejisikiruadesola@gmail.com>
2020-10-12 18:36:14 +0100

    Initialize

--- core/initialize.php
+++ core/initialize.php
@@ -2,6 +2,11 @@
 error_reporting(0);
 
 
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 class Initialize {
 
